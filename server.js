afda781e56d837637ea6ed35eda0b586d474c124
thomassmith0526 <thomassmith0526@gmail.com>
2024-06-24 16:43:57 -0500

    fsPromise

--- server.js
+++ server.js
@@ -2,24 +2,32 @@ const express = require("express");
 const fs = require("fs")
 const PORT = 3002;
 const path = require('path')
-
+const fsPromises = require('fs').promises;
 const app = express();
-const noteid = require("./express/public/helpers/id");
+const noteId = require("./express/public/helpers/id");
+const { json } = require("body-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// API paths
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/express/public/index.html"))
 );
 
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/express/public/notes.html'))
+);
+
+//API GET
 app.get("/api/notes", (req, res) => {
-  res.json(`${req.method} request received`);
+  fsPromises.readFile('./express/db/db.json','UTF-8', (err,data) => data)
+  .then((data) => res.json(JSON.parse(data)))
 
   console.info(`${req.method} request received to get notes`);
 });
-
+// API POST
 app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a note`);
   const { title, text } = req.body;
@@ -28,12 +36,15 @@ app.post("/api/notes", (req, res) => {
     const newNote = {
       title,
       text,
-      id: noteid(),
+      id: noteId(),
     };
 
-    const noteSrting = JSON.stringify(newNote);
+ 
 
-    fs.writeFile(`./express/db/db.json`, noteSrting, (err) =>
+    const noteString = JSON.stringify(newNote);
+    
+
+    fs.writeFile(`./express/db/db.json`, noteString, (err) =>
       err
         ? console.error(err)
         : console.log(
@@ -53,5 +64,7 @@ app.post("/api/notes", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Expree sever listening on port ${PORT}!`);
+  console.log(`Express sever listening on port ${PORT}!`);
 });
+
+
